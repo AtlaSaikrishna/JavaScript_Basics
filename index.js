017f6a6d8498c4487d02cfd88953f38a2734ff1b
AtlaSaikrishna <atlasaikrishna2@gmail.com>
2026-04-14 09:00:16 +0530

    Update commit

--- index.js
+++ index.js
@@ -1,41 +1,80 @@
-const obj = {
-    id : 1,
-    name : "saikrishna",
-    myData : [10,"hi","bye"]
-}
+// const obj = {
+//     id : 1,
+//     name : "saikrishna",
+//     myData : [10,"hi","bye"]
+// }
 
-console.log(obj.loc);
+// console.log(obj.loc);
 
-obj.loc = "hyd";
+// obj.loc = "hyd";
 
-console.log(obj);
+// console.log(obj);
 
-console.log(obj.myData);
+// console.log(obj.myData);
 
-const users = [
-    {
-        id : 1,
-        name : "sky",
-        loc : "hyd"
-    },
-    {
-        id:2,
-        name:"ram",
-        loc : "bng"
-    }
-]
+// const users = [
+//     {
+//         id : 1,
+//         name : "sky",
+//         loc : "hyd"
+//     },
+//     {
+//         id:2,
+//         name:"ram",
+//         loc : "bng"
+//     }
+// ]
 
-console.log(users[0].loc)
+// console.log(users[0].loc)
 
-for(let key in users){
-    console.log(key)
-}
+// for(let key in users){
+//     console.log(key)
+// }
 
-function myFunc(){
-    console.log(-1);
-    return -1;
+// function myFunc(){
+//     console.log(-1);
+//     return -1;
     
-}
+// }
 
-myFunc()
-console.log(myFunc());
+// myFunc()
+// console.log(myFunc());
+
+// const myFun = function add(n1,n2){
+//     return n1+n2;
+// }
+
+// console.log(myFun(50,20));
+
+// function add(n1,n2){
+//     return n1-n2;
+// }
+
+// function calculator(n1,n2,op){
+//     let res;
+//     switch(op){
+//         case "+" : res = n1+n2; break;
+//         case "-" : res = n1-n2; break;
+//         case "*" : res = n1*n2; break;
+//         case "/" :  res = n1/n2; break;
+//         case "%" : res = n1%n2; break;
+//         default : res = 0;
+//     }
+//     return res;
+// }
+
+// let n1 = 10
+// let n2 = 20
+// let op = "+"
+
+// console.log("The addition of "+n1+" & "+n2+" is "+calculator(n1,n2,op));
+
+
+//Arrow Functions => =>
+    let name = "krishna"
+    let age = 21
+
+    console.log("My name is "+name+". my age is "+age);
+    console.log(`My name is ${name}. my age is ${age}`);
+    
+    
